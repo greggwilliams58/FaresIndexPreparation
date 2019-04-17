3181486dd4f6752fd64582a1b67c87a30ac3b354
Greg Williams <greggwilliams58@hotmail.com>
2019-04-17 09:35:35 +0100

    added group and sum to main
    group and sum added to main for checking extraction

--- FaresIndexPreparation/main.py
+++ FaresIndexPreparation/main.py
@@ -36,6 +36,9 @@ def main():
     print("the superfile is coming....\n")
     exportfile(superfile,destinationpath,'superfile')
 
+    totalscheck = superfile.groupby(['Carrier TOC / Third Party Code'])['Adjusted Earnings Amount','Operating Journeys'].agg('sum')
+    exportfile(totalscheck,destinationpath,'sum_of_earnings_and_journies_by_toc')
+
     print("the advanced data is coming.... main\n")
     advanced = advanced_data(superfileforadvanced,destinationpath,LENNONadvancedfarepath)
     exportfile(advanced,destinationpath,'advancedfile')

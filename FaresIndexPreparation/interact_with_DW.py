0f794f7070af1bf4ddf779f658bbc12822bf5b15
Greg Williams <greggwilliams58@hotmail.com>
2019-11-06 09:36:11 +0000

    Interact with DW update
    The adding of generic rows functionalised as add new cat rows

--- FaresIndexPreparation/interact_with_DW.py
+++ FaresIndexPreparation/interact_with_DW.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy import create_engine, MetaData, Table, select, inspect
 from sqlalchemy.orm import sessionmaker
 import pprint as pp
+from commonfunctions import exportfile
 
 
 
@@ -62,75 +63,108 @@ def prep_template(df,type,RPI):
     #print("New values")
     #print(janvalues)
 
-    #amend the values of the subset for Average change in price
-    changeinprice = df[df['Year & stats']=='Average change in price (%)']
-    pricechangevalues = changeinprice.copy(deep=True)
+    avg_price_change = addnewrows(df,new_load_id,publication_status,'Average change in price (%)',latest_order,1)
+    exp_weights = addnewrows(df,new_load_id,publication_status,'Expenditure weights (%) total',latest_order,2)
+    yoy_change = addnewrows(df,new_load_id,publication_status,'Real terms change in average price year on year',latest_order,3)
+    yon2004change = addnewrows(df,new_load_id,publication_status,'Real terms change in average price year on 2004',latest_order,4)
 
-    pricechangevalues.loc[:,'Load_ID'] = new_load_id
-    pricechangevalues.loc[:,'Publication_status'] = publication_status
-    #sector remains the same
-    #ticket category remains the same
-    #ordering of ticket category remains the same
-    #year & stats remains the same
-    pricechangevalues.loc[:,'ordering of year & stats'] = latest_order + 1
-    pricechangevalues.loc[:,'value'] = None
+    ##amend the values of the subset for Average change in price
+    #changeinprice = df[df['Year & stats']=='Average change in price (%)']
+    #pricechangevalues = changeinprice.copy(deep=True)
 
-    #print(pricechangevalues)
+    #pricechangevalues.loc[:,'Load_ID'] = new_load_id
+    #pricechangevalues.loc[:,'Publication_status'] = publication_status
+    ##sector remains the same
+    ##ticket category remains the same
+    ##ordering of ticket category remains the same
+    ##year & stats remains the same
+    #pricechangevalues.loc[:,'ordering of year & stats'] = latest_order + 1
+    #pricechangevalues.loc[:,'value'] = None
+
+    ##print(pricechangevalues)
 
 
-    #amend the values of the subset for Expenditure Weights
-    expweights = df[df['Year & stats']=='Expenditure weights (%) total']
-    print(expweights)
-    expweightvalues = expweights.copy(deep=True)
+    ##amend the values of the subset for Expenditure Weights
+    #expweights = df[df['Year & stats']=='Expenditure weights (%) total']
+    #print(expweights)
+    #expweightvalues = expweights.copy(deep=True)
 
-    expweightvalues.loc[:,'Load_ID'] = new_load_id
-    expweightvalues.loc[:,'Publication_status'] = publication_status
-    #sector remains the same
-    #ticket category remains the same
-    #ordering of ticket category remains the same
-    #year & stats remains the same
-    expweightvalues.loc[:,'ordering of year & stats'] = latest_order + 2
-    expweightvalues.loc[:,'value'] = None
+    #expweightvalues.loc[:,'Load_ID'] = new_load_id
+    #expweightvalues.loc[:,'Publication_status'] = publication_status
+    ##sector remains the same
+    ##ticket category remains the same
+    ##ordering of ticket category remains the same
+    ##year & stats remains the same
+    #expweightvalues.loc[:,'ordering of year & stats'] = latest_order + 2
+    #expweightvalues.loc[:,'value'] = None
 
-    #print(expweightvalues)
+    ##print(expweightvalues)
 
     
-    #amend the values of the subset for YonY change
-    yoychange = df[df['Year & stats']=='Real terms change in average price year on year']
-    print(yoychange)
-    yoychangevalues = yoychange.copy(deep=True)
+    ##amend the values of the subset for YonY change
+    #yoychange = df[df['Year & stats']=='Real terms change in average price year on year']
+    #print(yoychange)
+    #yoychangevalues = yoychange.copy(deep=True)
 
-    yoychangevalues.loc[:,'Load_ID'] = new_load_id
-    yoychangevalues.loc[:,'Publication_status'] = publication_status
-    #sector remains the same
-    #ticket category remains the same
-    #ordering of ticket category remains the same
-    #year & stats remains the same
-    yoychangevalues.loc[:,'ordering of year & stats'] = latest_order + 3
-    yoychangevalues.loc[:,'value'] = None
+    #yoychangevalues.loc[:,'Load_ID'] = new_load_id
+    #yoychangevalues.loc[:,'Publication_status'] = publication_status
+    ##sector remains the same
+    ##ticket category remains the same
+    ##ordering of ticket category remains the same
+    ##year & stats remains the same
+    #yoychangevalues.loc[:,'ordering of year & stats'] = latest_order + 3
+    #yoychangevalues.loc[:,'value'] = None
 
-    #print(yoychangevalues)
+    ##print(yoychangevalues)
 
+    ##amend the values of the subset for Yon2004 change
+    #yo2004change = df[df['Year & stats']=='Real terms change in average price year on 2004']
+    #print(yo2004change)
+    #yo2004changevalues = yoychange.copy(deep=True)
+
+    #yo2004changevalues.loc[:,'Load_ID'] = new_load_id
+    #yo2004changevalues.loc[:,'Publication_status'] = publication_status
+    ##sector remains the same
+    ##ticket category remains the same
+    ##ordering of ticket category remains the same
+    ##year & stats remains the same
+    #yo2004changevalues.loc[:,'ordering of year & stats'] = latest_order + 4
+    #yo2004changevalues.loc[:,'value'] = None
+
+    ##print(yo2004changevalues)
+
+
+
+def addnewrows(fulldataset,new_load_id,publication_status,category,sortnumber,increment):
+    """
+    This is the add the rows for four generic items
+
+    Parameters
+    fulldataset:        A dataframe containing the full template from DW table
+    new_load_id:        An integer holding the new load_id
+    publication_status: A string holding the publication_status of the data
+    category:           A string containing the name of the category
+    sortnumber:         An integer to provide the base sort number
+    increment:          An integet to increment the sort number by
+
+
+    """
+    outputgoesto = outputto = 'C:\\Users\\gwilliams\\Desktop\\Python Experiments\\work projects\\FaresIndexSourceData\\advanced_and_non_advanced_output\\adv_non_advanced_and_superfile\\'
     #amend the values of the subset for Yon2004 change
-    yo2004change = df[df['Year & stats']=='Real terms change in average price year on 2004']
-    print(yo2004change)
-    yo2004changevalues = yoychange.copy(deep=True)
+    subset = fulldataset[fulldataset['Year & stats']==category ]
 
-    yo2004changevalues.loc[:,'Load_ID'] = new_load_id
-    yo2004changevalues.loc[:,'Publication_status'] = publication_status
+    subset = subset.copy(deep=True)
+
+    subset.loc[:,'Load_ID'] = new_load_id
+    subset.loc[:,'Publication_status'] = publication_status
     #sector remains the same
     #ticket category remains the same
     #ordering of ticket category remains the same
     #year & stats remains the same
-    yo2004changevalues.loc[:,'ordering of year & stats'] = latest_order + 4
-    yo2004changevalues.loc[:,'value'] = None
+    subset.loc[:,'ordering of year & stats'] = sortnumber + increment
+    subset.loc[:,'value'] = None
 
-    #print(yo2004changevalues)
-
-
-
-
-
+    exportfile(subset,outputgoesto,category)
 
 
     
